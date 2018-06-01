df8059a08d143a0a1d1c124000ca98cdced74f87
Wayne <liuzhy520@gmail.com>
2018-06-02 00:37:43 +0800

    xunfei demo added

--- app/src/main/java/liuzhy/voicecmmmanddemo/MainActivityActivity.java
+++ app/src/main/java/liuzhy/voicecmmmanddemo/MainActivityActivity.java
@@ -3,11 +3,15 @@ package liuzhy.voicecmmmanddemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
+
 public class MainActivityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        SpeechUtility.createUtility(this, SpeechConstant.APPID +"=5b0eadc9");
     }
 }
